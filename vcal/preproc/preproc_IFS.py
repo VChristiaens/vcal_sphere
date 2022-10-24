44aba3e70a214b587462f09b7c78c4ab77aa49de
IainHammond <Iain.Hammond@monash.edu>
2022-10-24 11:58:12 +1100

    added # coding: utf-8

--- vcal/preproc/preproc_IFS.py
+++ vcal/preproc/preproc_IFS.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
+# coding: utf-8
 
 """
 Module for pre-processing SPHERE/IFS data using VIP.
--- vcal/preproc/preproc_IRDIS.py
+++ vcal/preproc/preproc_IRDIS.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
+# coding: utf-8
 
 """
 Module with the preprocessing routine for SPHERE/IRDIS data.
