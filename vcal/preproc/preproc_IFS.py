3d395877d48eb634c4bc78cab9b0ba8ffaa7bf31
IainHammond <Iain.Hammond@monash.edu>
2022-11-02 11:09:00 +1100

    trying to fix memory

--- vcal/preproc/preproc_IFS.py
+++ vcal/preproc/preproc_IFS.py
@@ -348,16 +348,16 @@ def preproc_IFS(params_preproc_name='VCAL_params_preproc_IFS.json',
                             header["NAXIS2"] = cube.shape[2]
                         if 0 < bp_crop_sz < cube.shape[1]:
                             cube = cube_crop_frames(cube,bp_crop_sz)
-                        cube = cube_fix_badpix_clump(cube, bpm_mask=None, cy=None, cx=None, fwhm=1.2*resels, 
+                        cube = cube_fix_badpix_clump(cube, bpm_mask=None, cy=None, cx=None, fwhm=1.2*resels,
                                                      sig=6., protect_mask=0, verbose=full_output,
                                                      half_res_y=False, max_nit=max_bpix_nit, full_output=full_output)
                         if full_output:
                             write_fits(outpath+filename+"_1bpcorr_bpmap.fits", cube[1], header=header, verbose=debug)
                             cube = cube[0]
                         write_fits(outpath+filename+"_1bpcorr.fits", cube, header=header, verbose=debug)
-                        cube = None
-                        header = None
-                        print('{}MB'.format(getCurrentMemoryUsage()),flush=True)
+                    cube = None
+                    header = None
+                    print('{}MB'.format(getCurrentMemoryUsage()),flush=True)
                     
                     
         #******************************* RECENTERING ******************************
