9e9362523ceb9ae811f903dbf9f4ee68540307cf
IainHammond <Iain.Hammond@monash.edu>
2023-08-23 15:56:04 +1000

    plotting coordinates of satspots

--- vcal/preproc/preproc_IFS.py
+++ vcal/preproc/preproc_IFS.py
@@ -719,11 +719,15 @@ def preproc_IFS(params_preproc_name='VCAL_params_preproc_IFS.json',
                                         plt.close("all")
 
                                         fig, ax = plt.subplots()
-                                        ax.scatter(sat_x, sat_y)
+                                        for cc in range(true_ncen):
+                                            ax.scatter(sat_x[cc], sat_y[cc], label=f"Cube {cc+1}")
                                         ax.scatter(0, 0, color="black", marker="x")
+                                        ax.hline(0, color="black", ls="-", alpha=0.8)
+                                        ax.vline(0, color="black", ls="-", alpha=0.8)
                                         ax.minorticks_on()
-
+                                        ax.legend(loc="best")
                                         plt.savefig(outpath+"Satspot_coordinates.pdf", bbox_inches="tight")
+                                        plt.cloes("all")
 
                             elif "radon" in rec_met_tmp:
                                 cube, y_shifts, x_shifts = cube_recenter_radon(cube, full_output=True, verbose=True, imlib='opencv',
