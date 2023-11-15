1d96d7a4d2bfdf96ebb60009532c6dfb0d2512e8
IainHammond <Iain.Hammond@monash.edu>
2023-11-15 15:33:49 +1100

    placed shifts vs. time plot in a try/except

--- vcal/preproc/preproc_IRDIS.py
+++ vcal/preproc/preproc_IRDIS.py
@@ -888,28 +888,32 @@ def preproc_IRDIS(params_preproc_name='VCAL_params_preproc_IRDIS.json',
                             write_fits(outpath+"TMP_shifts_y{}_{}_{}.fits".format(labels[fi],filters[ff],rec_met_tmp), np.array(final_y_shifts))
                             write_fits(outpath+"TMP_shifts_x{}_{}_{}.fits".format(labels[fi],filters[ff],rec_met_tmp), np.array(final_x_shifts))
                         if fi != 1 and plot and not use_cen_only:
-                            f, (ax1) = plt.subplots(1,1, figsize=(15,10))
-                            t0 = np.amin(mjd_all)  # first file
-                            ax1.errorbar(#np.arange(1,len(file_list)+1,1./cube.shape[0]),
-                                         (mjd_all-t0)*60*24,  # convert to minutes since first file
-                                         final_y_shifts, final_y_shifts_std,
-                                         fmt='bo', label='y')
-                            ax1.errorbar(#np.arange(1,len(file_list)+1,1./cube.shape[0]),
-                                         (mjd_all-t0)*60*24,
-                                         final_x_shifts, final_x_shifts_std,
-                                         fmt='ro',label='x')
-                            if "satspots" in rec_met_tmp:
-                                t0 = np.amin(unique_mjd_cen)
-                                ax1.errorbar((unique_mjd_cen-t0)*60*24,y_shifts_cen,y_shifts_cen_err,
-                                             fmt='co',label='y cen')
-                                ax1.errorbar((unique_mjd_cen-t0)*60*24,x_shifts_cen,x_shifts_cen_err,
-                                             fmt='mo',label='x cen')
-                            ax1.set_xlabel("Time from start of obs. (min)")
-                            ax1.set_ylabel("Shift (px)")
-                            ax1.minorticks_on()
-                            plt.legend(loc='best')
-                            plt.savefig(outpath+"Shifts_xy{}_{}.pdf".format(labels[fi],rec_met_tmp),bbox_inches='tight', format='pdf')
-                            plt.clf()
+                            try:
+                                f, (ax1) = plt.subplots(1,1, figsize=(15,10))
+                                t0 = np.amin(mjd_all)  # first file
+                                ax1.errorbar(#np.arange(1,len(file_list)+1,1./cube.shape[0]),
+                                             (mjd_all-t0)*60*24,  # convert to minutes since first file
+                                             final_y_shifts, final_y_shifts_std,
+                                             fmt='bo', label='y')
+                                ax1.errorbar(#np.arange(1,len(file_list)+1,1./cube.shape[0]),
+                                             (mjd_all-t0)*60*24,
+                                             final_x_shifts, final_x_shifts_std,
+                                             fmt='ro',label='x')
+                                if "satspots" in rec_met_tmp:
+                                    t0 = np.amin(unique_mjd_cen)
+                                    ax1.errorbar((unique_mjd_cen-t0)*60*24,y_shifts_cen,y_shifts_cen_err,
+                                                 fmt='co',label='y cen')
+                                    ax1.errorbar((unique_mjd_cen-t0)*60*24,x_shifts_cen,x_shifts_cen_err,
+                                                 fmt='mo',label='x cen')
+                                ax1.set_xlabel("Time from start of obs. (min)")
+                                ax1.set_ylabel("Shift (px)")
+                                ax1.minorticks_on()
+                                plt.legend(loc='best')
+                                plt.savefig(outpath+"Shifts_xy{}_{}.pdf".format(labels[fi],rec_met_tmp),bbox_inches='tight', format='pdf')
+                                plt.clf()
+                            except:
+                                print('Could not produce shifts vs. time plot.', flush=True)
+                                plt.close('all')
                 
     
         #******************************* MASTER CUBES ******************************
