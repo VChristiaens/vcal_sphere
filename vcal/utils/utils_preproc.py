corrected theoretical scaling

vcal/utils/utils_preproc.py
@@ -357,7 +357,7 @@ def scaling_by_satspots(lbdas: np.array, coordinates_array: np.array, snr_channe
 
     low_snr_mask = snr_channel <= snr_thres
     if np.any(low_snr_mask):
-        scale_list_measured[low_snr_mask] = lbdas[low_snr_mask] / min(lbdas)
+        scale_list_measured[low_snr_mask] = max(lbdas) / lbdas[low_snr_mask]
         low_snr_channels = lbdas[low_snr_mask]
         print(f"Low SNR (<= {snr_thres}) for channels {low_snr_channels} µm, using theoretical scaling here")
         if np.sum(low_snr_mask) >= len(lbdas) * 2 / 3: