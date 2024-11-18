88d13e0088e5dada7d853cc629f768df84519f15
IainHammond <Iain.Hammond@monash.edu>
2024-11-18 12:10:41 +1100

    step 2: possibly fixed cross_corr

--- vcal/preproc/preproc_IRDIS.py
+++ vcal/preproc/preproc_IRDIS.py
@@ -858,14 +858,9 @@ def preproc_IRDIS(params_preproc_name='VCAL_params_preproc_IRDIS.json',
                                             if fn >= 1:
                                                 y_shifts = np.asanyarray(y_shi, dtype=object)
                                                 x_shifts = np.asanyarray(x_shi, dtype=object)
-                                                set_trace()
-                                                final_y_shifts = np.mean(
-                                                    y_shifts[((2 * ncen) * ((fn - 1) + 1)):((2 * ncen) * (fn + 1))],
-                                                    axis=0)
-                                                final_x_shifts = np.mean(
-                                                    x_shifts[((2 * ncen) * ((fn - 1) + 1)):((2 * ncen) * (fn + 1))],
-                                                    axis=0)
 
+                                                final_y_shifts = np.mean(y_shifts[((2 * ncen) * ((fn-1)+1)):((2 * ncen) * (fn+1))], axis=0)
+                                                final_x_shifts = np.mean(x_shifts[((2 * ncen) * ((fn-1)+1)):((2 * ncen) * (fn+1))], axis=0)
                                                 cube = cube_shift(cube, final_y_shifts[1:] + + np.mean(y_const),
                                                                   final_x_shifts[1:] + np.mean(x_const))
 
