42c17e13e9d04cf07e5b8a674ecd676a25ef704e
IainHammond <Iain.Hammond@monash.edu>
2024-11-18 15:23:17 +1100

    step 2: cross_corr has a bug

--- vcal/preproc/preproc_IRDIS.py
+++ vcal/preproc/preproc_IRDIS.py
@@ -859,11 +859,12 @@ def preproc_IRDIS(params_preproc_name='VCAL_params_preproc_IRDIS.json',
 
                                                 y_shifts = np.asanyarray(y_shi,dtype=object)
                                                 x_shifts = np.asanyarray(x_shi,dtype=object)
-                                                set_trace()
+
                                                 final_y_shifts = np.mean(y_shifts[((2 * ncen) * ((fn-1)+1)):((2 * ncen) * (fn+1))], axis=0)
                                                 final_x_shifts = np.mean(x_shifts[((2 * ncen) * ((fn-1)+1)):((2 * ncen) * (fn+1))], axis=0)
 
-                                                cube = cube_shift(cube, final_y_shifts[1:] + np.mean(y_const), final_x_shifts[1:] + np.mean(x_const))
+                                                cube = cube_shift(cube, final_y_shifts[1:] + np.mean(y_const),
+                                                                  final_x_shifts[1:] + np.mean(x_const))
 
 
                             elif "dft" in rec_met_tmp:
